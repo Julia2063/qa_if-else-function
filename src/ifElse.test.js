b9767752a8b8224e1c7c71b8da3798e73d221bc7
Julia2063 <sky206380@gmail.com.com>
2025-12-15 01:05:59 +0200

    add more robust checking

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -13,17 +13,21 @@ describe('ifElse', () => {
 
   it('should call second arg one time, if first arg return true', () => {
     const f = jest.fn();
+    const f2 = jest.fn();
 
-    ifElse(() => true, f, () => {});
+    ifElse(() => true, f, f2);
 
     expect(f).toHaveBeenCalledTimes(1);
+    expect(f2).not.toHaveBeenCalled();
   });
 
-  it('should call third arg one time, if first arg return true', () => {
+  it('should call third arg one time, if first arg return false', () => {
     const f = jest.fn();
+    const f2 = jest.fn();
 
-    ifElse(() => false, () => {}, f);
+    ifElse(() => false, f2, f);
 
     expect(f).toHaveBeenCalledTimes(1);
+    expect(f2).not.toHaveBeenCalled();
   });
 });
