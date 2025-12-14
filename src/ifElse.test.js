28d5e0afad23f641f25d49f0ec047178ada8e49d
Julia2063 <sky206380@gmail.com.com>
2025-12-15 00:59:09 +0200

    Solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,29 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should call first arg one time', () => {
+    const f = jest.fn();
 
+    ifElse(f, () => {}, () => {});
+
+    expect(f).toHaveBeenCalledTimes(1);
   });
 
-  // write tests here
+  it('should call second arg one time, if first arg return true', () => {
+    const f = jest.fn();
+
+    ifElse(() => true, f, () => {});
+
+    expect(f).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call third arg one time, if first arg return true', () => {
+    const f = jest.fn();
+
+    ifElse(() => false, () => {}, f);
+
+    expect(f).toHaveBeenCalledTimes(1);
+  });
 });
